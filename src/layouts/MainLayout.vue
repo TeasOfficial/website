4edd237e164bb9408e2136e4d4f3bb2dd0f9b3b3
OriginalSnow <70016672+IM2eR0@users.noreply.github.com>
2024-10-19 13:35:56 +0800

    Home Page & Main Style

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,48 +1,74 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR lfr">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          <center>
+            <q-btn flat padding="12.5px 24px" label="首页" to="/" />
+            <q-btn flat padding="12.5px 24px" label="喷漆文件制作器" href="https://nekogan.com/vtf/" />
+            <q-btn flat padding="12.5px 24px" label="服务器帮助文档" href="https://docs.nekogan.com/" />
+            <q-btn flat padding="12.5px 24px" label="BHOP查询" @click="alert = true" />
+          </center>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" side="right" overlay behavior="desktop" elevated>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
+        <q-item-label header>
+          相关链接
         </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+
+        <q-item-label header>
+          <center>
+            <nav>
+              Teas Official Website v2.0
+            </nav>
+            <nav>
+              Powered by NekoGan
+            </nav>
+          </center>
+        </q-item-label>
       </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer>
+      <center class="copyright">
+        Eh5の茶会 2020-2024
+        <br>
+        <a href="https://beian.miit.gov.cn/">辽ICP备2023013547号</a>
+        &nbsp;
+        <a href="https://beian.mps.gov.cn/#/query/webSearch?code=21052202000156">
+          <img width="10" src="https://beian.mps.gov.cn/web/assets/logo01.6189a29f.png">
+          辽公网安备21052202000156号
+        </a>
+      </center>
+    </q-footer>
   </q-layout>
+
+  <q-dialog v-model="alert" transition-show="flip-down" transition-hide="flip-up">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">提示</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        该页面仍在制作中，请耐心等待！
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="关闭" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup>
@@ -53,54 +79,55 @@ defineOptions({
   name: 'MainLayout'
 })
 
+const alert = ref(false)
+
 const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+
 ]
 
 const leftDrawerOpen = ref(false)
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
+
+<style scoped>
+.q-header {
+  background-color: rgb(250, 250, 250);
+  color: black;
+}
+
+.q-list {
+  background-color: rgb(250, 250, 250);
+  color: black;
+}
+
+.q-btn {
+  font-size: 16px;
+  color: black;
+}
+
+.q-footer center {
+  padding: 5px;
+  background-color: white;
+  color: black;
+}
+
+a {
+  color: #9b4dca;
+  text-decoration: none;
+  font-size: 12px;
+}
+
+.q-footer {
+  font-size: 15px;
+  background-color: white;
+}
+
+.copyright {
+  border-left: 0.3rem solid #d1d1d1;
+  border-right: 0.3rem solid #d1d1d1;
+  margin: 0 60px 60px 60px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <center>
+      <h1 style="margin-bottom: 20px;">Eh5の茶会</h1>
+      <nav>一个友善温馨的GMod小服务器</nav>
+      <nav>同时为广大服务器提供各类服务</nav>
+      <hr>
+      <h3 style="margin-bottom: 0;">🌟 服务导航 🌟</h3>
+      <nav><i>PS：部分服务需点击上方 <b>服务器帮助文档</b> 进行使用方式查询阅读</i></nav>
+      <div>
+        <ol class="daohanglan">
+          <li><a target="_blank" href="http://gmod.ltd:54010/airs/">Airs度假休闲歌单生成器</a></li>
+          <li><a target="_blank" href="http://gmod.ltd:54030">NekoVerify</a></li>
+          <li><a target="_blank" href="https://gccf.nekogan.com/">GModCEFCodecFix 中文镜像版</a></li>
+          <li><a target="_blank" href="https://nekogan.com/vtf">喷漆文件制作器</a></li>
+          <li><a target="_blank" href="https://glxy.nekogan.com">自助查服机器人注册网站</a></li>
+          <li><a target="_blank" href="https://nekogan.com/steam">SteamID 查询器</a></li>
+          <li><a target="_blank" href="https://dr.nekogan.com">zhaochangjack的死跑服数据查询网</a></li>
+          <li><a target="_blank" href="https://ncm.nekogan.com">网易云音乐API</a></li>
+          <li><a target="_blank" href="https://github.com/IM2eR0/CloudMusicGMOD-Unofficial">网易云音乐播放器非官方延续版（GMOD）</a>
+          </li>
+          <li><a target="_blank" href="https://www.glxy30.com">静态网站托管服务</a></li>
+        </ol>
+      </div>
+      <hr>
+    </center>
   </q-page>
 </template>
 
@@ -13,3 +32,31 @@ defineOptions({
   name: 'IndexPage'
 });
 </script>
+
+<style scoped>
+.daohanglan {
+  margin-top: 1rem;
+}
+
+blockquote,
+dl,
+figure,
+form,
+ol,
+p,
+pre,
+table,
+ul {
+  margin-bottom: 2.5rem;
+}
+
+ol {
+  list-style: decimal inside;
+  padding: 0;
+}
+
+a {
+  color: #9b4dca;
+  text-decoration: none;
+}
+</style>
